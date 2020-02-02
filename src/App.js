73723ee3d3f5b016e61a46d7ea6e9c86efcd4d85
shafali03 <shaf03sm@gmail.com>
2020-02-02 11:24:57 +0000

    created a state variable that has property call count

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Counter from './Counter'
 
 function App() {
   return (
-    <Counter initialCounter={0} />
+    <Counter initialCounter={5} />
   )
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      count: props.initialCount
+    }
+  }
+
+
   render() {
     return (
       <div>
